c869394ac92f4fa472a9bdf7769b8a39a553aec4
RishiChaurasia <rishi.cse09@gmail.com>
2022-03-30 20:00:08 +0530

    added generics for network call

--- LBGAssignment/Network/ServiceRequestor.swift
+++ LBGAssignment/Network/ServiceRequestor.swift
@@ -15,15 +15,14 @@ protocol MovieListServiceRequestorProtocol {
     /// ```
     /// - Warning: The function returns  an optional value also can throw an exception
     /// - Parameter method:  - Service Request method which will  define which method needs to be called
-    /// - Parameter requestType:  - Data request type which will decide to request for test data or live data
+    /// - Parameter isMockRequest: - Fetch for mock data or real data
     /// - Parameter responseType: - Mock data response type, decides the nature of returned object in mock enviroment
-    ///
     /// - Returns: A tuple of  optional Movies Model and optional error.
     ///
 
     func getMoviesList(searchString: String,
                        method: ServiceRequestMethod,
-                       requestType: DataRequestType?,
+                       isMockRequest: Bool?,
                        responseType: MockDataResponseType?) async throws -> (movieModelArray: [Movies]?, error: Error?)
 
     /// This function is responsible for fetching movie list and returns  a tuple of  optional Movies Model and error
@@ -33,14 +32,13 @@ protocol MovieListServiceRequestorProtocol {
     /// ```
     /// - Warning: The function returns  an optional value also can throw an exception
     /// - Parameter method:  - Service Request method which will  define which method needs to be called
-    /// - Parameter requestType:  - Data request type which will decide to request for test data or live data
+    /// - Parameter isMockRequest: - Fetch for mock data or real data
     /// - Parameter responseType: - Mock data response type, decides the nature of returned object in mock enviroment
-    ///
     /// - Returns: A tuple of  optional Movies Model and optional error.
     ///
 
     func fetchAndParseMovieData(urlRequest: URLRequest,
-                                requestType: DataRequestType?,
+                                isMockRequest: Bool?,
                                 responseType: MockDataResponseType?,
                                 method: ServiceRequestMethod?) async throws -> (movieModelArray: [Movies]?, error: Error?)
 
@@ -50,7 +48,7 @@ struct MovieListServiceRequestor: MovieListServiceRequestorProtocol {
 
     func getMoviesList(searchString: String,
                        method: ServiceRequestMethod,
-                       requestType: DataRequestType? = nil,
+                       isMockRequest: Bool? = false,
                        responseType: MockDataResponseType? = nil) async throws -> (movieModelArray: [Movies]?, error: Error?) {
         var urlString = ServiceRequestUtility().getURLStringForMethod(method: method)
         urlString = "\(urlString)\(searchString)"
@@ -61,7 +59,7 @@ struct MovieListServiceRequestor: MovieListServiceRequestorProtocol {
         urlRequest.httpMethod = ApiRequestType.get.rawValue
         do {
             return try await fetchAndParseMovieData(urlRequest: urlRequest,
-                                                    requestType: requestType,
+                                                    isMockRequest: isMockRequest,
                                                     responseType: responseType,
                                                     method: method)
         } catch let error {
@@ -71,20 +69,16 @@ struct MovieListServiceRequestor: MovieListServiceRequestorProtocol {
     }
 
     func fetchAndParseMovieData(urlRequest: URLRequest,
-                                requestType: DataRequestType? = nil,
+                                isMockRequest: Bool? = false,
                                 responseType: MockDataResponseType? = nil,
                                 method: ServiceRequestMethod? = nil) async throws -> (movieModelArray: [Movies]?, error: Error?) {
         var moviesArray = [Movies]()
         do {
-            let response =  try await NetworkLayer.initiateServiceRequest(request: urlRequest,
-                                                             requestType: requestType,
-                                                             responseType: responseType,
-                                                             method: method)
+            let response =  try await NetworkLayer.initiateServiceRequest(request: urlRequest, resultType: MovieResponse.self, isMockRequest: isMockRequest, mockResponseType: responseType, method: method )
             guard let responseData = response.responseData else {
                 return (nil, response.serviceError)
             }
-            let results  =  try JSONDecoder().decode(MovieResponse.self, from: responseData)
-            for movie in results.results {
+            for movie in responseData.results {
                 moviesArray.append(movie)
             }
         } catch let error {
@@ -96,6 +90,7 @@ struct MovieListServiceRequestor: MovieListServiceRequestorProtocol {
 }
 
 struct NetworkLayer {
+
     /// This function is responsible to initialize network call with URLSession and returns a tuple of  optional Movies Model and optional error.
     /// ```
     /// initiateServiceRequest
@@ -108,24 +103,30 @@ struct NetworkLayer {
     ///
     ///
 
-   static func initiateServiceRequest(request: URLRequest,
-                                      requestType: DataRequestType? = nil,
-                                      responseType: MockDataResponseType? = nil,
-                                      method: ServiceRequestMethod? = nil) async throws -> (responseData: Data?, serviceError: Error?) {
+    static func initiateServiceRequest<T: Decodable>(request: URLRequest,
+                                                     resultType: T.Type,
+                                                     isMockRequest: Bool? = false,
+                                                     mockResponseType: MockDataResponseType? = nil,
+                                                     method: ServiceRequestMethod? = nil) async throws -> (responseData: T?, serviceError: Error?) {
         if !ConnectionManager.hasConnectivity() {
             return (nil, CustomError.connectionFailed)
         }
         do {
-            if requestType == .testData {
-                let mockData = try await getMockData(responseType: responseType, method: method)
-                return mockData
+            if isMockRequest == true {
+                let mockData = try await getMockData(responseType: mockResponseType, method: method)
+                if let _mockResponse = mockData.responseData {
+                    let results  =  try JSONDecoder().decode(T.self, from: _mockResponse)
+                    return (results, nil)
+                }
+                return (nil, CustomError.unexpected)
             } else {
                 let (serverData, serverUrlResponse) = try await URLSession.shared.data(for: request)
                 guard let httpStausCode = (serverUrlResponse as? HTTPURLResponse)?.statusCode,
                       (200...299).contains(httpStausCode) else {
                     return (nil, CustomError.unexpected)
                 }
-                return (serverData, nil)
+                let results  =  try JSONDecoder().decode(T.self, from: serverData)
+                return (results, nil)
             }
         } catch let error {
             debugPrint(error.localizedDescription)
